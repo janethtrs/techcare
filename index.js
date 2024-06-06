e7a8a67c460875d8699225d9cc4b1e0a4782bb4c
janethtrs98@gmail.com <janethtrs98@gmail.com>
2024-06-06 16:33:06 -0700

    fetch patients

--- index.js
+++ index.js
@@ -9,6 +9,36 @@ const requestOptions = {
 };
 
 fetch("https://fedskillstest.coalitiontechnologies.workers.dev", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
+  .then((response) => response.json())
+  .then(data => {
+    patients(data);
+  })
   .catch((error) => console.error(error));
+
+  function patients(users){
+    const user = document.getElementById('patientProfile');
+
+    users.forEach(patient => {
+      const patientDiv = document.createElement('div');
+      patientDiv.className = 'patient';
+
+      const patientInfo =
+      `
+      <div class="patient-profile">
+    <img src="${patient.profile_picture}" width="48" height="48" alt="profileImage" />
+    <div class="labels">
+      <label>${patient.name}</label>
+      <label class="job">${patient.gender},${patient.age}</label>
+    </div>
+    <img
+      width="18"
+      height="4"
+      src="/Pictures/morehorizontalIcon.svg"
+      alt="morehorizontalIcon"
+    />
+  </div>
+      `;
+      patientDiv.innerHTML= patientInfo;
+      user.appendChild(patientDiv);
+    })
+  }
