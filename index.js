7449a0b98e1fdc498685efe86d1a880b5400f590
janethtrs98@gmail.com <janethtrs98@gmail.com>
2024-07-12 16:33:57 -0700

    chart with dataset

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ fetch("https://fedskillstest.coalitiontechnologies.workers.dev", requestOptions)
   .then((response) => response.json())
   .then(data => {
     patients(data);
+    history(data);
   })
   .catch((error) => console.error(error));
 
@@ -35,6 +36,7 @@ fetch("https://fedskillstest.coalitiontechnologies.workers.dev", requestOptions)
       height="4"
       src="/Pictures/morehorizontalIcon.svg"
       alt="morehorizontalIcon"
+      class="moreIcon"
     />
   </div>
       `;
@@ -42,3 +44,68 @@ fetch("https://fedskillstest.coalitiontechnologies.workers.dev", requestOptions)
       user.appendChild(patientDiv);
     })
   }
+
+function abbreviateMonth(month) {
+    const monthAbbreviations = {
+      October: "Oct",
+      November: "Nov",
+      December: "Dec",
+      January: "Jan",
+      February: "Feb",
+      March: "Mar",
+    };
+
+    return monthAbbreviations[month] || month; // Devuelve la abreviatura o el mes original si no se encuentra en el objeto
+}
+  function history(diagnosis,date,blood_pressure_diastolic,blood_pressure_systolic){
+    const ctx = document.getElementById('myChart');
+
+    blood_pressure_diastolic = 
+    [
+      diagnosis[3].diagnosis_history[5].blood_pressure.diastolic.value,
+      diagnosis[3].diagnosis_history[4].blood_pressure.diastolic.value,
+      diagnosis[3].diagnosis_history[3].blood_pressure.diastolic.value,
+      diagnosis[3].diagnosis_history[2].blood_pressure.diastolic.value,
+      diagnosis[3].diagnosis_history[1].blood_pressure.diastolic.value,
+      diagnosis[3].diagnosis_history[0].blood_pressure.diastolic.value,
+    ]
+    blood_pressure_systolic =
+    [
+      diagnosis[3].diagnosis_history[5].blood_pressure.systolic.value,
+      diagnosis[3].diagnosis_history[4].blood_pressure.systolic.value,
+      diagnosis[3].diagnosis_history[3].blood_pressure.systolic.value,
+      diagnosis[3].diagnosis_history[2].blood_pressure.systolic.value,
+      diagnosis[3].diagnosis_history[1].blood_pressure.systolic.value,
+      diagnosis[3].diagnosis_history[0].blood_pressure.systolic.value,
+    ]
+    dates = 
+    [
+    diagnosis[3].diagnosis_history[5].month+ ', '+diagnosis[3].diagnosis_history[5].year,
+    diagnosis[3].diagnosis_history[4].month+ ', '+diagnosis[3].diagnosis_history[4].year,
+    diagnosis[3].diagnosis_history[3].month+ ', '+diagnosis[3].diagnosis_history[3].year,
+    diagnosis[3].diagnosis_history[2].month+ ', '+diagnosis[3].diagnosis_history[2].year,
+    diagnosis[3].diagnosis_history[1].month+ ', '+diagnosis[3].diagnosis_history[1].year,
+    diagnosis[3].diagnosis_history[0].month+ ', '+diagnosis[3].diagnosis_history[0].year,
+    ]
+
+    const formattedDates = dates.map(date=> {
+      const [fullMonthName, year] = date.split(', ');
+      const abbreviateMonthName = abbreviateMonth(fullMonthName.trim());
+      return abbreviateMonthName + ', ' + year;
+    })
+
+    new Chart(ctx, {
+        data: {
+            datasets: [{
+                type: 'line',
+                label: 'Blood pressure diastolic',
+                data: blood_pressure_diastolic,
+            }, {
+                type: 'line',
+                label: 'Blood pressure systolic',
+                data: blood_pressure_systolic,
+            }],
+            labels:formattedDates
+        },
+    });
+  };
